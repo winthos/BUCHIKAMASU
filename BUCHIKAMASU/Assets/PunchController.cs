b22b87adf05efbe97331c07a019a4ee8b2b9f1b3
winthos <winson.whan@gmail.com>
2018-03-08 17:51:25 -0800

    controller inpuuuut

--- BUCHIKAMASU/Assets/PunchController.cs
+++ BUCHIKAMASU/Assets/PunchController.cs
@@ -9,6 +9,18 @@ public class PunchController : MonoBehaviour
     public bool isAxisInUse = false;
     public bool isAxisInUse_Straight = false;
 
+    public bool isAxisInUse_LeftHook = false;
+    public bool LeftHookAct1 = false;
+
+    public bool isAxisInUse_RightHook = false;
+    public bool RightHookAct1 = false;
+
+    public bool isAxisInUse_LeftUpper = false;
+    public bool LeftUpperAct1 = false;
+
+    public bool isAxisInUse_RightUpper = false;
+    public bool RightUpperAct1 = false;
+
 	// Use this for initialization
 	void Start () 
     {
@@ -20,13 +32,13 @@ public class PunchController : MonoBehaviour
     {
         isAxisInUse = false;
     }
-	
-	// Update is called once per frame
-	void Update () 
-    {
-        //print(Input.GetAxisRaw("RightStickVertical"));
 
-		if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.F))
+    // Update is called once per frame
+    void Update()
+    {
+        //print(Input.GetAxisRaw("RightStickHorizontal"));
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.F))
         {
             MyAnimator.SetTrigger("Jab");
         }
@@ -62,8 +74,9 @@ public class PunchController : MonoBehaviour
 
         }
 
-        //////////////CONTROLLER//////////////////
+        //////////////CONTROLLER NONSENSE//////////////////
 
+        //JAB!//
         if (Input.GetAxis("LeftStickVertical") <= -0.5)
         {
             if (isAxisInUse == false)
@@ -78,16 +91,14 @@ public class PunchController : MonoBehaviour
             isAxisInUse = false;
         }
 
-        //////
-
+        ////STRAIGHTO//
         if (Input.GetAxis("RightStickVertical") <= -0.5)
         {
             if (isAxisInUse_Straight == false)
             {
                 isAxisInUse_Straight = true;
-               // print("buh");
                 MyAnimator.SetTrigger("Straight");
-                
+
             }
         }
 
@@ -95,5 +106,112 @@ public class PunchController : MonoBehaviour
         {
             isAxisInUse_Straight = false;
         }
+
+        //LEFT HOOK/////-1 on left to 1 on right
+        if (Input.GetAxis("LeftStickHorizontal") <= -0.5)
+        {
+            if(isAxisInUse_LeftHook == false)
+            {
+               // print("buh");
+                isAxisInUse_LeftHook = true;
+                LeftHookAct1 = true;
+                StartCoroutine("LeftHookCountDown");
+               
+            }
+        }
+
+        if(Input.GetAxis("LeftStickHorizontal") >= 0.5 && LeftHookAct1 == true)
+        {
+            LeftHookAct1 = false;
+            MyAnimator.SetTrigger("LeftHook");
+
+        }
+
+        //RIGHT HOOOOK////
+        if (Input.GetAxis("RightStickHorizontal") >= 0.5)
+        {
+            if (isAxisInUse_RightHook == false)
+            {
+                // print("buh");
+                isAxisInUse_RightHook = true;
+                RightHookAct1 = true;
+                StartCoroutine("RightHookCountDown");
+            }
+        }
+
+        if (Input.GetAxis("RightStickHorizontal") <= -0.5 && RightHookAct1 == true)
+        {
+            RightHookAct1 = false;
+            MyAnimator.SetTrigger("RightHook");
+        }
+
+        ////LEFT UPPPAAAAAA/////
+        if (Input.GetAxis("LeftStickVertical") >= 0.5)
+        {
+            if (isAxisInUse_LeftUpper == false)
+            {
+                isAxisInUse_LeftUpper = true;
+                LeftUpperAct1 = true;
+                StartCoroutine("LeftUpperCountDown");
+            }
+        }
+
+        if (Input.GetAxis("LeftStickVertical") <= -0.5 && LeftUpperAct1 == true)
+        {
+            LeftUpperAct1 = false;
+            MyAnimator.SetTrigger("LeftUpper");
+        }
+
+
+        ////RIGHT UPPPAAAAA/////
+
+        if (Input.GetAxis("RightStickVertical") >= 0.5)
+        {
+            if (isAxisInUse_RightUpper == false)
+            {
+                isAxisInUse_RightUpper = true;
+                RightUpperAct1 = true;
+                StartCoroutine("RightUpperCountDown");
+            }
+        }
+
+        if (Input.GetAxis("RightStickVertical") <= -0.5 && RightUpperAct1 == true)
+        {
+            RightUpperAct1 = false;
+            MyAnimator.SetTrigger("RightUpper");
+        }
+
+    }
+
+    IEnumerator LeftHookCountDown()
+    {
+        yield return new WaitForSeconds(0.05f);
+
+        LeftHookAct1 = false;
+        isAxisInUse_LeftHook = false;
+    }
+
+    IEnumerator RightHookCountDown()
+    {
+        yield return new WaitForSeconds(0.05f);
+
+        RightHookAct1 = false;
+        isAxisInUse_RightHook = false;
+    }
+
+    IEnumerator LeftUpperCountDown()
+    {
+        yield return new WaitForSeconds(0.15f);
+
+        LeftUpperAct1 = false;
+        isAxisInUse_LeftUpper = false;
+    }
+
+    IEnumerator RightUpperCountDown()
+    {
+        yield return new WaitForSeconds(0.15f);
+
+        RightUpperAct1 = false;
+        isAxisInUse_RightUpper = false;
     }
 }
